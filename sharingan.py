c2021bea3f37567f0af331ca2cfe17d784b94add
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-08 18:38:26 -0600

    Update sharingan.py

--- sharingan.py
+++ sharingan.py
@@ -2,3 +2,13 @@
 # Fecha: 9/10/24
 # Descripción: Código para usar turtle.
 # GitHub: https://github.com/Jona163
+
+from turtle import *
+begin_poly()
+pencolor('red')
+dot(750)
+pencolor('black')
+dot(500)
+pencolor('red')
+dot(475)
+#Funcion curva01
