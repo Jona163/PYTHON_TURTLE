48d34cc8159929fc5926fd044a9f41c619f8d8f5
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-08 18:39:13 -0600

    Update sharingan.py

--- sharingan.py
+++ sharingan.py
@@ -22,3 +22,18 @@ def curve02(a,d):
   forward(1)
 pencolor('black')
 #Funcion para el sharingan
+def shr(n,p):
+ right(n)
+ penup()
+ forward(230)
+ pendown()
+ right(p)
+ fillcolor("black")
+ begin_fill()
+ curve02(1.3,200)
+ right(125)
+ curve01(1,60)
+ left(160)
+ curve02(0.85,180)
+ end_fill()
+ #Llamado del shard.
