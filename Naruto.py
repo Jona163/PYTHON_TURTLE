8d22d9876ea044d5641d59195c74ca49db47c7e7
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-08 18:40:17 -0600

    Update Naruto.py

--- Naruto.py
+++ Naruto.py
@@ -2,3 +2,32 @@
 # Fecha: 9/10/24
 # Descripción: Código para usar turtle.
 # GitHub: https://github.com/Jona163
+
+from turtle import *
+pencolor("red")
+pensize(8)
+hideturtle()
+
+pencolor('black')
+
+
+left(13)
+speed(8)
+fillcolor("yellow")
+begin_fill()
+penup()
+forward(190)
+pendown()
+
+#Hair
+right(25)
+forward(60)
+left(135)
+forward(100)
+right(95)
+forward(95)
+left(135)
+forward(110)
+right(105)
+forward(115)
+left(135)
